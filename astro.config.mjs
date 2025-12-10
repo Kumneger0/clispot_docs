326efc5f2ac83c265667d337d1405822672e451c
kumneger0 <kumnegerwondimu01@gmail.com>
2025-12-10 20:50:03 +0300

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,30 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Clispot Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kumneger0/clispot' }],
-			sidebar: [
-				{
-					label: 'Core',
-					items: [
-						{ label: 'Installation', slug: 'core/installation' },
-						{ label: 'Configuration', slug: 'core/configuration' },
-						{ label: 'Features', slug: 'core/features' },
-						{ label: 'Usage', slug: 'core/usage' },
-						{ label: 'Troubleshooting', slug: 'core/troubleshooting' },
-					],
-				},
-				{
-					label: 'VS Code Extension',
-					items: [
-						{ label: 'Quickstart', slug: 'vscode-extension/quickstart' },
-					],
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'Clispot Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/kumneger0/clispot' }],
+        sidebar: [
+            {
+                label: 'Core',
+                items: [
+                    { label: 'Installation', slug: 'core/installation' },
+                    { label: 'Configuration', slug: 'core/configuration' },
+                    { label: 'Features', slug: 'core/features' },
+                    { label: 'Usage', slug: 'core/usage' },
+                    { label: 'Troubleshooting', slug: 'core/troubleshooting' },
+                ],
+            },
+            {
+                label: 'VS Code Extension',
+                items: [
+                    { label: 'Quickstart', slug: 'vscode-extension/quickstart' },
+                ],
+            },
+        ],
+		}), sitemap()],
 });
